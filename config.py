db9ac6b2cb8735e5bb3fe92ce7a7758eedf16a40
wbbeyourself <beyourselfwb@163.com>
2024-07-12 10:15:57 +0800

    update

--- config.py
+++ config.py
@@ -4,9 +4,9 @@
 STAR_KEYWORDS = ['Text-to-SQL', 'Retrieval-Augmented Generation', 'Multimodal', 'RAG', 'Survey']
 
 # add your authors here
-_AUTHOR_GROUP1 = ['Zhoujun Li', 'Wanxiang Che', 'Zhiyuan Liu', 'Dawn Song', 'Kaiming He', 'Min-Yen Kan', 'Jian-Guang Lou', 'Yan Gao', 'Jiajun Zhang', 'Ji-Rong Wen', 'Zhicheng Dou']
-_AUTHOR_GROUP2 = ['Qian Liu', 'Libo Qin', 'Li Dong', 'Xinyun Chen', 'Tao Yu', 'Danqi Chen', 'Diyi Yang', 'Jason Wei', 'Wanjun Zhong', 'Xinnian Liang', 'Shuangzhi Wu', 'Binyuan Hui', 'Jian Yang', 'Yanlin Wang', 'Bei Chen', 'Nan Duan', 'Ensheng Shi', 'Bowen Yu', 'Jiaqi Guo', 'Xi Victoria Lin', 'Tianbao Xie', 'Pengcheng Yin', 'Yao Fu', 'Hongcheng Guo', 'Xin Zhao', 'Zhiruo Wang', 'Yifei Li', 'Hui Huang']
-_AUTHOR_GROUP3 = ['Jiuxiang Gu']
+_AUTHOR_GROUP1 = ['Zhoujun Li', 'Wanxiang Che', 'Zhiyuan Liu', 'Dawn Song', 'Kaiming He', 'Min-Yen Kan', 'Jian-Guang Lou', 'Yan Gao', 'Jiajun Zhang', 'Ji-Rong Wen', 'Wayne Xin Zhao', 'Zhicheng Dou']
+_AUTHOR_GROUP2 = ['Qian Liu', 'Libo Qin', 'Li Dong', 'Xinyun Chen', 'Tao Yu', 'Danqi Chen', 'Diyi Yang', 'Jason Wei', 'Wanjun Zhong', 'Xinnian Liang', 'Shuangzhi Wu', 'Binyuan Hui', 'Jian Yang', 'Yanlin Wang', 'Bei Chen', 'Nan Duan', 'Ensheng Shi', 'Bowen Yu', 'Jiaqi Guo', 'Xi Victoria Lin', 'Tianbao Xie', 'Pengcheng Yin', 'Yao Fu', 'Hongcheng Guo', 'Xin Zhao', 'Zhiruo Wang', 'Yifei Li', 'Hui Huang', 'Junyi Li']
+_AUTHOR_GROUP3 = ['Jinyang Li', 'Mohammadreza Pourreza']
 STAR_AUTHORS = _AUTHOR_GROUP1 + _AUTHOR_GROUP2 + _AUTHOR_GROUP3
 
 # add your meetings here
